94f7e75bd4ab4e8ef513fea3116da5179f59a36e
Diones <pereiradiones987@gmail.com>
2024-10-29 22:57:55 -0300

    criando interceptador para gerar novo token

--- src/api/gerencianet.js
+++ src/api/gerencianet.js
@@ -12,10 +12,10 @@ const agent = new https.Agent({
   passphrase: ''
 });
 
-const credentials = Buffer.from(`${process.env.GN_CLIENT_ID}:${process.env.GN_CLIENTE_SECRET}`).toString('base64')
+const credentials = Buffer.from(`${process.env.GN_CLIENT_ID}:${process.env.GN_CLIENTE_SECRET}`).toString('base64');
 
+// Função para obter o token de autenticação
 const authenticate = () => {
-  console.log('HELLO');
   return axios({
     method: 'POST',
     url: `${process.env.GN_ENDPOINT}/oauth/token`,
@@ -31,18 +31,46 @@ const authenticate = () => {
 }
 
 const GNRequest = async () => {
-  const authResponse = await authenticate();
-  const acessToken = authResponse.data?.access_token;
+  let authResponse = await authenticate();
+  let accessToken = authResponse.data?.access_token;
 
-  return axios.create({
+  const instance = axios.create({
     baseURL: process.env.GN_ENDPOINT,
     httpsAgent: agent,
     headers: {
-      Authorization: `Bearer ${acessToken}`,
-      'ContentType': 'application/json'
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json'
     }
   });
+
+  // Interceptor de resposta para detectar expiração de token
+  instance.interceptors.response.use(
+    (response) => response, // Passa a resposta normalmente se não houver erro
+    async (error) => {
+      const originalRequest = error.config;
+
+      // Se o erro for 401, significa que o token expirou
+      if (error.response && error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true; // Marca a requisição para evitar loops infinitos
+
+        // Gera um novo token
+        authResponse = await authenticate();
+        accessToken = authResponse.data?.access_token;
+
+        // Atualiza o cabeçalho de autorização com o novo token
+        instance.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
+        originalRequest.headers['Authorization'] = `Bearer ${accessToken}`;
+
+        // Reenvia a requisição original com o novo token
+        return instance(originalRequest);
+      }
+
+      return Promise.reject(error); // Retorna o erro se não for 401
+    }
+  );
+
+  return instance;
 }
-//Falta implementar o refresh token
+
 
 module.exports = GNRequest;
